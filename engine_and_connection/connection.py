9dbc0bdbfc6a33c4fdb41884047d17971013309a
MarcusV <Marcus-v@outlook.com>
2023-02-11 12:59:16 -0300

    basic connect and execute method

--- engine_and_connection/connection.py
+++ engine_and_connection/connection.py
@@ -1,3 +1,16 @@
-import SQLAlchemy
+from sqlalchemy import create_engine, text
+from decouple import config
+
+# the motor that specific the dialet
+engine = create_engine(f"mysql+{config('DRIVER')}"
+                       f"://{config('USER')}"
+                       f":@{config('ADDRESS')}"
+                       f":{config('PORT', cast=int)}"
+                       f"/{config('DATABASE')}")
+connection = engine.connect()
+response = connection.execute(text("SELECT * FROM movies;"))
+
+for row in response:
+    print(row.title)
 
 
